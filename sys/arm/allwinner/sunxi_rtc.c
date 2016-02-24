79e53357a3ac90ab6c171bc47c8bdceb0542775b
fate <fate@vm.r2d2.ru>
2016-02-24 02:51:53 +0000

    Committer: digitalfate <fate10@gmail.com>
     On branch user/digitalfate/arm
     Changes to be committed:
    	modified:   sunxi_rtc.c
    
     Changes not staged for commit:
    	modified:   files.allwinner

--- sys/arm/allwinner/sunxi_rtc.c
+++ sys/arm/allwinner/sunxi_rtc.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) _____________
+ * Copyright (c) 2016 Vladimir Belian <fate10@gmail.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
